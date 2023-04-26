b4848dad68252a14444b1a4f1adedcf119dcc520
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-04-26 22:30:27 +0500

    Add items to homepage from API

--- src/modules/displayItems.js
+++ src/modules/displayItems.js
@@ -1,3 +1,27 @@
-// API about movie series was selected for this project
+const items = document.querySelector("#items");
+const url = "https://api.tvmaze.com/shows";
 
-// const url = 'https://api.tvmaze.com/shows';
+export default async () => {
+  const response = await fetch(url);
+  const itemData = await response.json();
+  itemData.forEach((data) => {
+    items.innerHTML +=
+      data.id >= 204 && data.id <= 209
+        ? `
+        <div class="row">
+            <img src="${data.image.medium}"/>
+            <h1>${data.name}</h1>
+            <i data-id="${data.id}" class="fa icons">&#xf087;</i>
+            <div class="numOfLikes">
+              <p class="likesCount likes" data-id="${data.id}">0</p>
+              <p class="likes">likes</p>
+            </div>
+            <div class="buttons">
+                <button type="submit" class="comment button">Comments</button>
+                <br>
+                <button type="submit" class="reserve button">Reservations</button>
+            </div>
+        </div>`
+        : "";
+  });
+};
