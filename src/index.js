940965124a2e7f0bd3558591d714f55a05ad69a5
AFakhriddinov <112176053+AFakhriddinov@users.noreply.github.com>
2023-04-27 09:06:42 +0500

    Display number of likes on homepage after like icon is clicked

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import './style.css';
-import listItems from './modules/displayItems.js';
+import "./style.css";
+import listItems from "./modules/displayItems.js";
+import addLikes from "./modules/addLikes.js";
 
-window.addEventListener('load', (event) => {
+window.addEventListener("load", (event) => {
   listItems();
+  addLikes();
 });
