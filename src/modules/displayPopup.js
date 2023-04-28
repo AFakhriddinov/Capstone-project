4c2f44e8c4c52d373b0b8c09318d97dc8b220493
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2023-04-28 09:04:06 +0300

    Fix linter errors

--- src/modules/displayPopup.js
+++ src/modules/displayPopup.js
@@ -1,18 +1,18 @@
-import { moviesArray } from "./displayItems.js";
+import { moviesArray } from './displayItems.js';
 
-export const popup = async () => {
-    const body = document.querySelector("body");
-    const popupElement = document.createElement("div");
-    let county = 0;
-    moviesArray.forEach((movie) => {
-      const moviePopup = `
+const popup = async () => {
+  const body = document.querySelector('body');
+  const popupElement = document.createElement('div');
+  let county = 0;
+  moviesArray.forEach((movie) => {
+    const moviePopup = `
       <div class="popup">
           <img class="close" onclick="this.parentNode.style.display = 'none';" src="https://static.vecteezy.com/system/resources/thumbnails/020/048/793/small/x-transparent-background-free-png.png">
           <img class="popImg" src="${movie.image.original}">
           <h1 class="name">${movie.name}</h1>
           <h4 class="genres"><span class="bold"> Movie genres:</span> ${movie.genres.join(
-            ", "
-          )}</h4>
+    ', ',
+  )}</h4>
           <div class="summary">${movie.summary}</div>
           <div class="commentContainer">
           &nbsp;Comments&nbsp;[<span class="commentsCount">0</span>]
@@ -30,20 +30,21 @@ export const popup = async () => {
           </form>
       </div>
       `;
-      displayComment(`"${movie.id}"`, county);
-  
-      county += 1;
-      popupElement.innerHTML += moviePopup;
+    // displayComment(`"${movie.id}"`, county);
+
+    county += 1;
+    popupElement.innerHTML += moviePopup;
+  });
+  body.appendChild(popupElement);
+};
+
+window.setTimeout(() => {
+  const commentBtn = document.querySelectorAll('.comment');
+
+  for (let i = 0; i < commentBtn.length; i += 1) {
+    commentBtn[i].addEventListener('click', () => {
+      document.querySelectorAll('.popup')[i].style.display = 'flex';
     });
-    body.appendChild(popupElement);
-  };
-  
-  window.setTimeout(() => {
-    const commentBtn = document.querySelectorAll(".comment");
-  
-    for (let i = 0; i < commentBtn.length; i += 1) {
-      commentBtn[i].addEventListener("click", () => {
-        document.querySelectorAll(".popup")[i].style.display = "flex";
-      });
-    }
-  }, 2000);
+  }
+}, 2000);
+export default popup;
